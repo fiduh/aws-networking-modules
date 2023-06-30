538fc72f8fe1b891477cd47bd69ee813a97179bf
francis iduh <fiduh987@gmail.com>
2023-06-30 18:52:01 +0100

    add private route table output and nat gateway output to modules/vpc/outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,6 +1,9 @@
 
-output "public_route_table_id" {
-  value = aws_route_table.public[0].id
+output "private_route_table_id" {
+  value = aws_route_table.rt_for_single_nat[0]
   description = "AWS Route Table ID, to enable aditional routes to be added externally"
 }
 
+output "nat_gatway_id" {
+  value = aws_nat_gateway.single_nat[0].id
+}
