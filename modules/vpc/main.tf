659bcf5ef6f0179b27cd1517d93d5651f30a62ee
francis iduh <fiduh987@gmail.com>
2023-06-30 12:35:16 +0100

    add private route table

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -87,6 +87,20 @@ resource "aws_subnet" "private" {
   }
 }
 
+# Private Route Tables
+resource "aws_route_table" "private_rt" {
+  count = length(var.private_subnets_cidr_with_azs) != 0 ? 1 : 0
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "private-route-table"
+  }
+}
+resource "aws_route_table_association" "private_rt_per_subnet" {
+  count = length(var.private_subnets_cidr_with_azs)
+  subnet_id      = local.private_subnets_ids[count.index]
+  route_table_id = aws_route_table.private_rt[0].id
+}
+# Private Route Table Subnets association
 
 # Private Route Tables for NATs per AZs
 resource "aws_route_table" "rt_for_nat_per_az" {
@@ -122,7 +136,7 @@ resource "aws_route" "private_rt_nat_entry_per_az" {
 
 }
 
-
+# Private Route Table for single NAT 
 resource "aws_route_table" "rt_for_single_nat" {
   count  = var.enable_single_nat ? 1 : 0
   vpc_id = aws_vpc.vpc.id
@@ -131,6 +145,7 @@ resource "aws_route_table" "rt_for_single_nat" {
   }
 }
 
+# Private Route Table Route entry for single NAT
 resource "aws_route" "nat_single_route" {
   count                  = var.enable_single_nat ? 1 : 0
   route_table_id         = aws_route_table.rt_for_single_nat[0].id
@@ -139,6 +154,7 @@ resource "aws_route" "nat_single_route" {
   depends_on             = [aws_route_table.rt_for_single_nat]
 }
 
+# Private Route Table Subnets association for single NAT
 resource "aws_route_table_association" "nat_single_subnet" {
   count          = var.enable_single_nat ? length(var.private_subnets_cidr_with_azs) : 0
   subnet_id      = local.private_subnets_ids[count.index]
@@ -166,7 +182,7 @@ resource "aws_internet_gateway" "igw" {
 # NAT Gateway
 ################################################################################
 
-
+# Single NAT Gateway
 resource "aws_nat_gateway" "single_nat" {
   count = var.enable_single_nat ? 1 : 0
 
@@ -182,17 +198,19 @@ resource "aws_nat_gateway" "single_nat" {
   depends_on = [aws_internet_gateway.igw]
 }
 
-
+# Elastic IP for Single NAT Gateway
 resource "aws_eip" "nat" {
   count  = var.enable_single_nat ? 1 : 0
   domain = "vpc"
 }
 
+# Elastic IPs for NAT Gateway Per AZs
 resource "aws_eip" "eip_nat_per_az" {
   count  = var.one_nat_gateway_per_az ? length(var.public_subnets_cidr_with_azs) : 0
   domain = "vpc"
 }
 
+# NAT Gateways for each Public Subnet per AZ
 resource "aws_nat_gateway" "nat_per_az" {
   count         = var.one_nat_gateway_per_az ? length(local.public_subnets_ids) : 0
   allocation_id = aws_eip.eip_nat_per_az[count.index].id
@@ -206,6 +224,5 @@ resource "aws_nat_gateway" "nat_per_az" {
   # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.igw]
 }
-/*
-*/
+
 
