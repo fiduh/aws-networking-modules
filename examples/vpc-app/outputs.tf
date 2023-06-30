ec2c3efcfe1f5a4dd326be4f2c84d36667597459
francis iduh <fiduh987@gmail.com>
2023-06-30 18:50:26 +0100

    add private route table output and nat gateway id to examples/vpc-app/outputs.tf

--- examples/vpc-app/outputs.tf
+++ examples/vpc-app/outputs.tf
@@ -1,4 +1,9 @@
 
-output "public_route_table_id" {
-  value = module.vpc-app-mgmt-datastore.public_route_table_id
+output "private_route_table_id" {
+  value = module.vpc-app-mgmt-datastore.private_route_table_id
+}
+
+
+output "nat_gatway_id" {
+  value = module.vpc-app-mgmt-datastore.nat_gatway_id
 }
