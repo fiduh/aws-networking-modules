e325e10bb9eb41f4f40492d85323cf15d1d227ec
francis iduh <fiduh987@gmail.com>
2023-07-05 17:25:56 +0100

    add security group outputs modules/vpc/outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,11 +1,10 @@
-/*
-output "private_route_table_id" {
-  value = aws_route_table.rt_for_single_nat[0]
-  description = "AWS Route Table ID, to enable aditional routes to be added externally"
+
+output "private_sg" {
+  value = aws_security_group.private_sg.id
+  description = "Private security group id"
 }
 
-output "nat_gatway_id" {
-  value = aws_nat_gateway.single_nat[0].id
+output "public_sg" {
+  value = aws_security_group.public_sg.id
+  description = "Private security group id"
 }
-
-*/
