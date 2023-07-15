797b5a057053e9c6c43f11926a832cecce5c70f0
francis iduh <fiduh987@gmail.com>
2023-07-15 13:58:15 +0100

    dynamically generate subner cidrs examples/vpc-app/main.tf

--- examples/vpc-app/main.tf
+++ examples/vpc-app/main.tf
@@ -25,17 +25,18 @@ data "aws_availability_zones" "available_azs" {
 
 locals {
   azs = slice(data.aws_availability_zones.available_azs.names, 0, 2)
+  vpc_cidr = "10.0.0.0/21"
 }
 
 module "microservices-vpc" {
   # source = "github.com/osemiduh/aws-networking-modules//modules/vpc?ref=v0.1.2"
   source         = "../../modules/vpc"
   vpc_name       = "microservice-vpc"
-  vpc_cidr_block = "10.0.0.0/21"
+  vpc_cidr_block = local.vpc_cidr
   azs = local.azs
-  public_subnets = ["10.0.0.0/24","10.0.1.0/24"]
-  private_subnets = ["10.0.2.0/24", "10.0.3.0/24"]
-
+  
+  public_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 3, k)]
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 3, k + 3)]
 
   enable_single_nat      = true
   one_nat_gateway_per_subnet = false
